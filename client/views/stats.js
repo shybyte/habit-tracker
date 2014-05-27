876ca5170bc0b89e10cc9379e4a2121666fb90a3
shybyte <ein@volloeko.de>
2014-05-27 21:00:19 +0200

    Stats should not show whole month if there is only data for the last couple of days.

--- client/views/stats.js
+++ client/views/stats.js
@@ -1,38 +1,47 @@
+function getLastMonthData() {
+  var lastMonthActions = Actions.find({
+    date: {$gte: moment().subtract('days', 31).toDate()}
+  }, {sort: {date: 1}}).fetch();
+
+  var now = moment();
+  var firstActionInLastMonth = lastMonthActions[0];
+  if (!firstActionInLastMonth) {
+    return [];
+  }
+  var daysSinceFirstActionInLastMonth = Math.floor((Date.now() - firstActionInLastMonth.date.getTime()) / 1000 / 60 / 60);
+  var lastMonthRange = Number.range(Math.min(31, daysSinceFirstActionInLastMonth + 1), 0).every();
+  var actionsByCategory = lastMonthActions.groupBy(function (action) {
+    return Habits.findOne({_id: action.habit}).category;
+  });
+  var data = Object.keys(actionsByCategory).map(function (categoryId) {
+    return {
+      values: lastMonthRange.map(function (daysAgo) {
+        var dayX = moment(now).subtract('days', daysAgo);
+        var actionsOnDayX = actionsByCategory[categoryId].filter(function (action) {
+          return moment(action.date).dayOfYear() == dayX.dayOfYear();
+        });
+        return {x: dayX.toDate(), y: actionsOnDayX.sum('duration')}
+      }),
+      key: categoryId
+    };
+  }).map(function (dataSet) {
+    dataSet.key = Categories.findOne({_id: dataSet.key}).title;
+    return dataSet
+  });
+  return data;
+}
+
 Template.stats.rendered = function () {
   var self = this;
   if (!self.drawStats) {
     self.drawStats = Meteor.autorun(function () {
-      var lastMonthActions = Actions.find({
-        date: {$gte: moment().subtract('days', 31).toDate()}
-      }, {sort: {date: -1}}).fetch();
-
-      var lastMonthRange = Number.range(31, 0).every();
-      var actionsByCategory = lastMonthActions.groupBy(function (action) {
-        return Habits.findOne({_id: action.habit}).category;
-      });
-      var now = moment();
-      var data = Object.keys(actionsByCategory).map(function (categoryId) {
-        return {
-          values: lastMonthRange.map(function (daysAgo) {
-            var dayX = moment(now).subtract('days', daysAgo);
-            var actionsOnDayX = actionsByCategory[categoryId].filter(function (action) {
-              return moment(action.date).dayOfYear() == dayX.dayOfYear();
-            });
-            return {x: dayX.toDate(), y: actionsOnDayX.sum('duration')}
-          }),
-          key: categoryId
-        };
-      }).map(function (dataSet) {
-        dataSet.key = Categories.findOne({_id: dataSet.key}).title;
-        return dataSet
-      });
-
+      var data = getLastMonthData();
       nv.addGraph(function () {
         var chart = nv.models.multiBarChart().stacked(true)
           .transitionDuration(350)
           .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
           .rotateLabels(0)      //Angle to rotate x-axis labels.
-          .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+          .showControls(data.length > 1)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
           .groupSpacing(0.1);    //Distance between each group of bars.
 
         chart.xAxis
