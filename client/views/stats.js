2fa09ff5b8bc65e2222a47fad9a8a0a79d0cf436
shybyte <ein@volloeko.de>
2014-05-30 16:52:42 +0200

    Added pie chart for last month

--- client/views/stats.js
+++ client/views/stats.js
@@ -22,23 +22,29 @@ function getLastMonthData() {
         });
         return {x: dayX.toDate(), y: actionsOnDayX.sum('duration')}
       }),
+      value: actionsByCategory[categoryId].sum('duration'),
       key: Categories.findOne({_id: categoryId}).title
     };
   });
 }
+
+
 
 Template.stats.rendered = function () {
   var self = this;
   if (!self.drawStats) {
     self.drawStats = Meteor.autorun(function () {
       var data = getLastMonthData();
+
       nv.addGraph(function () {
         var chart = nv.models.multiBarChart().stacked(true)
           .transitionDuration(350)
           .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
           .rotateLabels(0)      //Angle to rotate x-axis labels.
           .showControls(data.length > 1)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
-          .groupSpacing(0.1);    //Distance between each group of bars.
+          .groupSpacing(0.1)    //Distance between each group of bars.
+          .showYAxis(true);
+
 
         chart.xAxis
           .tickFormat(function (d) {
@@ -46,13 +52,32 @@ Template.stats.rendered = function () {
           });
 
         chart.yAxis
-          .tickFormat(d3.format(',.1f'));
+          .axisLabel('Minutes')
+          .tickFormat(d3.format(',.0f'));
+
+        chart.yAxis.axisLabelDistance(50);
 
         d3.select('#chart svg')
           .datum(data)
           .call(chart);
 
         nv.utils.windowResize(chart.update);
+      });
+
+      nv.addGraph(function() {
+        var chart = nv.models.pieChart()
+          .x(function(d) { return d.key; })
+          .y(function(d) { return d.value; })
+          .showLabels(true);
+
+        chart.tooltipContent(function (key, y, e, graph) {
+          return Math.floor(y / 6)/10 + ' hours';
+        });
+
+        d3.select("#monthPie svg")
+          .datum(data)
+          .transition().duration(350)
+          .call(chart);
       });
 
     });
